50bbdfb0bbff4b591162291d377213ccf9bd72df
Andy <andyarbem@gmail.com>
2022-08-26 13:55:55 +0800

    Add files

---
+++ 01LinearRegression/LinearRegression.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+# 求解y = wx + b
+# 计算总的偏差
+def compute_error_for_line_given_points(b, w, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (w * x + b)) ** 2
+    return totalError / float(len(points))
+
+
+# 求梯度信息
+def step_gradient(b_cur, w_cur, points, learningRate):
+    b_gradient = 0
+    w_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2 / N) * (y - ((w_cur * x) + b_cur))
+        w_gradient += -(2 / N) * x * (y - ((w_cur * x) + b_cur))
+    new_b = b_cur - (learningRate * b_gradient)
+    new_w = w_cur - (learningRate * w_gradient)
+    return [new_b, new_w]
+
+
+# 区间迭代求解
+def gradient_descent_runner(points, starting_b, starting_w,
+                            learning_rate, num_iterations):
+    b = starting_b
+    w = starting_w
+    for i in range(num_iterations):
+        b, w = step_gradient(b, w, np.array(points), learning_rate)
+    return [b, w]
+
+
+def run():
+    points = np.genfromtxt("data.csv", delimiter=",")
+    learning_rate = 0.0001
+    initial_b = 0  # initial y-intercept guess
+    initial_w = 0  # initial slope guess
+    num_iterations = 1000
+    print("Starting gradient descent at b = {0}, w = {1}, error = {2}"
+          .format(initial_b, initial_w,
+                  compute_error_for_line_given_points(initial_b, initial_w, points))
+          )
+    print("Running...")
+    [b, w] = gradient_descent_runner(points, initial_b, initial_w, learning_rate, num_iterations)
+    print("After {0} iterations b = {1}, w = {2}, error = {3}".
+          format(num_iterations, b, w,
+                 compute_error_for_line_given_points(b, w, points))
+          )
+
+
+if __name__ == '__main__':
+    run()
